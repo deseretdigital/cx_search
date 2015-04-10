27ac304f5fb2882248c23de8987856340590cd96
AndriyHudyma <ahud@softserveinc.com>
2015-04-10 15:37:20 +0200

    Add getFacetByName() method

--- tests/Cxsearch/ResultTest.php
+++ tests/Cxsearch/ResultTest.php
@@ -23,4 +23,34 @@ class ResultTest extends \PHPUnit_Framework_TestCase
         // assert
         $this->assertEquals($expected, $actual, 'Result object did not return expected value.');
     }
+
+    public function testFacetByName()
+    {
+        // arrange
+        $categories = new \stdClass();
+        $categories->id = 1;
+
+        $facets = new \stdClass();
+        $facets->categories = array($categories);
+
+        $indexMock = new \stdClass();
+        $resultset = new \stdClass();
+        $resultset->facets = $facets;
+        $resultset->start = 0;
+        $resultset->totalMatched = 10;
+        $resultset->matches = array();
+        $resultObj = new Result($indexMock, $resultset);
+
+        $expected = array (
+            0 =>array(
+                'id' => 1
+                )
+            );
+
+        // act
+        $actual = $resultObj->getFacetByName('categories');
+
+        // assert
+        $this->assertEquals($expected, $actual, 'Getting facets by name is not working');
+    }
 }
